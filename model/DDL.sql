c6a3c20da475d25789900b3cd978a473c1621a0b
Henrique Cavalieri <henrique.cavalieri@leaodiesel.com.br>
2023-03-09 14:24:03 -0300

    Basic database DDL

--- model/DDL.sql
+++ model/DDL.sql
@@ -1,3 +1,46 @@
-create database portal-kpi
+create table if not exists aux_emp_usuario
+(
+    usuario varchar(20) not null,
+    empresa int         not null,
+    padrao  varchar(1)  not null,
+    primary key (usuario, empresa)
+    );
 
---create table usuario
+create table if not exists aux_perfil_usuario
+(
+    perfil  int         not null,
+    usuario varchar(20) not null,
+    primary key (perfil, usuario)
+    );
+
+create table if not exists empresa
+(
+    codigo int         not null
+    primary key,
+    nome   varchar(50) not null
+    );
+
+create table if not exists perfil
+(
+    id_perfil int         not null
+    primary key,
+    descricao varchar(50) not null
+    );
+
+create table if not exists setor
+(
+    id_setor  int auto_increment
+    primary key,
+    descricao varchar(50) not null
+    );
+
+create table if not exists usuario
+(
+    usuario  varchar(20)            not null
+    primary key,
+    senha    varchar(50)            not null,
+    nome     varchar(50)            not null,
+    id_setor int        default 1   not null,
+    ativo    varchar(1) default 'A' not null
+    );
+
